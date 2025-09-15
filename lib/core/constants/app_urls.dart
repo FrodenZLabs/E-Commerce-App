1896dfe04e53b05da338385168f779cddf23385f
kibestevie <frodenzlabs@gmail.com>
2025-09-15 21:41:40 +0300

    Added app urls

---
+++ lib/core/constants/app_urls.dart
@@ -0,0 +1,6 @@
+class AppUrls {
+  static const storageBase = '';
+  static const categoryImage = '${storageBase}Categories%2FImages%2F';
+  static const productImage = '${storageBase}Products%2FImages%2F';
+  static const alt = '?alt=media';
+}
