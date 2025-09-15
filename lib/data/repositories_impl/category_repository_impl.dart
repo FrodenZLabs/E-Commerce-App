60671d00cd29948f8b48bc1d016617e2feeaa9b2
kibestevie <frodenzlabs@gmail.com>
2025-09-15 20:15:11 +0300

    Added category remote repository implementation.

---
+++ lib/data/repositories_impl/category_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/data/data_sources/remote/category_remote_data_source.dart';
+import 'package:e_commerce_app/data/models/category/category_model.dart';
+import 'package:e_commerce_app/domain/repository/category/category_repository.dart';
+
+class CategoryRepositoryImpl implements CategoryRepository {
+  final CategoryRemoteDataSource remoteDataSource;
+
+  CategoryRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either> getCategories() async {
+    final categories = await remoteDataSource.getCategories();
+    return categories.fold(
+      (error) {
+        return Left(error);
+      },
+      (data) {
+        return Right(
+          List.from(
+            data,
+          ).map((e) => CategoryModel.fromMap(e).toEntity()).toList(),
+        );
+      },
+    );
+  }
+}
