9c7df33ab50de9ed3db969329fb829c63166f7d7
kibestevie <frodenzlabs@gmail.com>
2025-09-15 18:25:35 +0300

    Added categories use cases

---
+++ lib/domain/usecases/category/get_categories_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_app/core/usecase/usecase.dart';
+import 'package:e_commerce_app/domain/repository/category/category_repository.dart';
+
+class GetCategoriesUseCase implements UseCase<Either, dynamic> {
+  final CategoryRepository repository;
+
+  GetCategoriesUseCase(this.repository);
+
+  @override
+  Future<Either> call({params}) async {
+    return await repository.getCategories();
+  }
+}
