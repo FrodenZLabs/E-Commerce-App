d25a4b4189eb3859e982408c68425ea797587756
kibestevie <frodenzlabs@gmail.com>
2025-09-17 01:26:32 +0300

    Added product card UI

---
+++ lib/presentation/widgets/product_card.dart
@@ -0,0 +1,88 @@
+import 'package:e_commerce_app/core/configs/theme/app_colors.dart';
+import 'package:e_commerce_app/domain/entity/product/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  const ProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // AppNavigator.push(context, ProductDetailPage());
+      },
+      child: Container(
+        width: 180,
+        decoration: BoxDecoration(
+          color: AppColors.secondBackground,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: NetworkImage(product.images[0]),
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    topRight: Radius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title,
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          product.discountedPrice == 0
+                              ? "${product.price}"
+                              : "${product.discountedPrice}",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          product.discountedPrice == 0
+                              ? ""
+                              : "${product.price}",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
